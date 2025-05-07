b6ef52c2616ffc99e0b4cd53d70141ef0787161d
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-07 16:16:02 +0000

    colors scss

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeHeaderComponent } from "../../components/home-header/home-header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [HomeHeaderComponent, FooterComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
