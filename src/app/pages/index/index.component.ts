4f3080c8ae6ef3e20de6367f63637f72723203ba
williancordeiro <82521979+williancordeiro@users.noreply.github.com>
2025-05-11 15:28:02 +0000

    b

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { HomeHeaderComponent } from "../../components/home-header/home-header.component";
+import { HomeComponent } from "../home/home.component";
+import { ProfileComponent } from "../profile/profile.component";
 
 @Component({
   selector: 'app-index',
-  imports: [FooterComponent, HomeHeaderComponent],
+  imports: [FooterComponent, HomeHeaderComponent, HomeComponent, ProfileComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
