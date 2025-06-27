8bafe80de38a36f153cfa8bfc5be44eab941667e
Willian Cordeiro <williancordeiro369@gmail.com>
2025-06-26 21:34:36 -0300

    update add-serie page

--- src/app/pages/add-serie/add-serie.component.ts
+++ src/app/pages/add-serie/add-serie.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AddSerieFormComponent } from '../../components/add-serie-form/add-serie-form.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-serie',
@@ -8,5 +9,10 @@ import { AddSerieFormComponent } from '../../components/add-serie-form/add-serie
   styleUrl: './add-serie.component.scss'
 })
 export class AddSerieComponent {
-
+  
+  constructor(private router: Router) {}
+  
+  goBack() {
+    this.router.navigate(['/index/home']);
+  }
 }
